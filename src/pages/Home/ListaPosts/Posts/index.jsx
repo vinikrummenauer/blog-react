138b72c654f0109a357bf6f158e805564f970684
vinikrummenauer <viniciuskdepaula@gmail.com>
2022-09-26 11:25:54 -0300

    att

--- src/pages/Home/ListaPosts/Posts/index.jsx
+++ src/pages/Home/ListaPosts/Posts/index.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "./styles.css";
 
 const Posts = ( obj ) => {
-  const {blog: 
+  const {blog:
     { description, title, thumbImageAltText, postDate, profileName, profileThumbImage, thumbImage }} = obj
   const { id } = obj
   return (
@@ -19,6 +19,8 @@ const Posts = ( obj ) => {
             <p>{postDate}</p>
           </div>
         </div>
+        
+        {/*!!!!!!!!!!!!!!!!!!!!!!*/}
         <Link className="blogItem-link" to={`/postagens/${id}`}>
           Ler mais
         </Link>
